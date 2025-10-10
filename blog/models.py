e00a89aeb90138c9c5ed130b74074d4f30ee2a9c
Alireza.N <naderkhani.alireza@gmail.com>
2025-10-10 11:16:47 +0330

    create blog model (tag / post / comment)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    tag = models.SlugField(max_length=50,unique=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=True)
+    recommended_score = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post , on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
