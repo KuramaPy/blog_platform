4658d6003b5712781849943879410b4e8b75c4bb
Alireza.N <naderkhani.alireza@gmail.com>
2025-10-10 10:40:15 +0330

    make user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    
+    """
+    # base fields
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+    birthdate = models.DateField(null=True, blank=True)
+    is_email_verified = models.BooleanField(default=False)
+    email_verification_token = models.UUIDField(default=uuid.uuid4, editable=False)
+    gfa_secret = models.CharField(max_length=32 , blank=True , null=True)
+    gfa_is_enabled = models.BooleanField(default=False)
+    profile_pic = models.ImageField(upload_to='profile_pic')
+    avatar = models.ImageField(upload_to='avatar')
+    
+    # more datail for users
+    bio = models.TextField(max_length=500, blank=True , null=True)
+    location = models.CharField(max_length=15, blank=True)
+    website = models.URLField(blank=True)
+    # social fields
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    social_links = models.JSONField(default=dict, blank=True)
+    # suggestion fields
+    insterests = models.ManyToManyField('blog.Tag', related_name='user_interests', blank=True)
+    # prefrences / securety fields 
+    prefrences = models.JSONField(default=dict, blank=True)
+    last_login_ip = models.GenericIPAddressField(blank=True, null=True)
+    
+    
+    def __str__(self):
+        return f"{self.username}'s Profile"
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
+        ordering = ['-date_joined']
--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -120,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media Root
+MEDIA_URL = '/media/'       # trailing slash is needed.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
